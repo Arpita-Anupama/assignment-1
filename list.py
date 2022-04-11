38d01718eebd0ebb9adf75b6364a6c96a4c404e1
Arpita Anupama <101637215+Arpita-Anupama@users.noreply.github.com>
2022-04-11 19:12:04 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,29 @@
+
+list1 = [1, 2, 3, 4]
+print(list1[1])
+print(list1[1:4])
+print(list1[-1])
+
+
+list1 =[]
+for i in range(0, 11):
+     list1.append(i)
+ 
+
+for i in range(0, 11):
+     print(list1[i])
+
+
+list1 =[1, 2, 3, 4]
+list1[2]= 5
+print(list1)
+
+
+list1.extend([1, 2, 3])
+print(list1)
+
+
+list1 = [1, 2, 3, 4, 5]
+print(list1)
+del list1[2]
+print(list1)
