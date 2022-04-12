6ee199dfbd85cf54423010e35697de7beab9c163
Arpita Anupama <101637215+Arpita-Anupama@users.noreply.github.com>
2022-04-12 17:50:52 +0530

    Add files via upload

---
+++ list modified.py
@@ -0,0 +1,49 @@
+#list
+fruits = ['apple', 'banana', 'cherry']
+fruits.append("orange")
+
+#append
+a = ["apple", "banana", "cherry"]
+b = ["Ford", "BMW", "Volvo"]
+a.append(b)
+
+#clear
+fruits = ['apple', 'banana', 'cherry', 'orange']
+fruits.clear()
+
+#copy
+fruits = ['apple', 'banana', 'cherry', 'orange']
+x = fruits.copy()
+
+#count
+fruits = ['apple', 'banana', 'cherry']
+x = fruits.count("cherry")
+
+#extend
+fruits = ['apple', 'banana', 'cherry']
+cars = ['Ford', 'BMW', 'Volvo']
+fruits.extend(cars)
+
+#index
+fruits = ['apple', 'banana', 'cherry']
+x = fruits.index("cherry")
+
+#insert
+fruits = ['apple', 'banana', 'cherry']
+fruits.insert(1, "orange")
+
+#pop
+fruits = ['apple', 'banana', 'cherry']
+fruits.pop(1)
+
+#remove
+fruits = ['apple', 'banana', 'cherry']
+fruits.remove("banana")
+
+#reverse
+fruits = ['apple', 'banana', 'cherry']
+fruits.reverse()
+
+#sort
+cars = ['Ford', 'BMW', 'Volvo']
+cars.sort()
